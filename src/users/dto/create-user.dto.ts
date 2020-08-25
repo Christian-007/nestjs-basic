b0d966b5cfc82b20cd723d2695d9ce7cf45edd56
Christian Ing Sunardi <christianingsunardi@gmail.com>
2020-08-25 22:14:51 +0700

    feat: implement ValidationPipe() for create()

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 30)
   firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 30)
   lastName: string;
 }
